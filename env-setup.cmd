305faf457b52a1fcdaa1b377d813d84c16e28d00
Gupta <ngupta1@dbar1.qtcm.com>
2021-10-21 13:24:31 -0700

    Angular and Node Env-Setup

---
+++ env-setup.cmd
@@ -0,0 +1,42 @@
+##update or install node-js and npm
+https://nodejs.org/en/download/
+
+
+### update or install latest angular clip
+
+
+# npm uninstall -g @angular/cli
+npm npm install -g @angular/cli@latest
+
+###install lerna or pnpm
+
+npm npm install -g lerna 
+
+npm npm install -g pnpm 
+
+##make lessons directoty
+
+md lessons 
+
+md lessons/lesson01
+md lessons/lesson02
+md lessons/lesson03
+md lessons/lesson04
+md lessons/lesson05
+md lessons/lesson06
+md lessons/lesson07
+md lessons/lesson08
+md lessons/lesson09
+md lessons/lesson10
+md lessons/lesson11
+md lessons/lesson12
+md lessons/lesson13
+md lessons/lesson14
+md lessons/lesson14
+
+md lessons/final 
+
+
+
+
+
