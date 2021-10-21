f2419028ac38e6a5b9014bc93541f037c12d5447
Gupta <ngupta1@dbar1.qtcm.com>
2021-10-21 16:25:37 -0700

    add Angular Forms to support ngModal-end

--- lesson00/src/app/app.module.ts
+++ lesson00/src/app/app.module.ts
@@ -3,8 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { SheetV1Component } from './sheet-v1/sheet.component';
 import { SheetV2Component } from './sheet-v2/sheet.component';
+
+
 
 @NgModule({
   declarations: [
@@ -13,7 +18,7 @@ import { SheetV2Component } from './sheet-v2/sheet.component';
     SheetV2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CommonModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
