6d31995ea7094d5eff8217d1a072648b1e380923
Gupta <ngupta1@dbar1.qtcm.com>
2021-10-22 08:20:22 -0700

    make dropdown - ready

--- lesson00/src/app/data/sampleColumns.ts
+++ lesson00/src/app/data/sampleColumns.ts
@@ -1,7 +1,16 @@
+// column configs
+// type can be text or dropdown or html input types
+// for dropdown using the of dict property from all lookups
 export const sampleColumnConfig = [
   { id: 1, name: 'id' , title:'Id' , type: 'text' , size:'20px' ,style:'' , lookup: ''},
   { id: 2, name: 'name', title: 'Full Name', type: 'text', size: '120px', style: '', lookup: ''},
-  { id: 3, name: 'cityId', title: 'City Name', type: 'text', size: '120px', style: '', lookup: '' },
-  { id: 4, name: 'fruitId', title: 'Poplular Food', type: 'text', size: '120px', style: '', lookup: '' }
+  {
+    id: 3, name: 'cityId', title: 'City Name', type: 'dropdown',
+    size: '120px', style: '', lookup: 'city'
+  },
+  {
+    id: 4, name: 'fruitId', title: 'Poplular Food', type: 'dropdown',
+    size: '120px', style: '', lookup: 'fruits'
+  }
 
 ]
--- lesson00/src/app/sheet-v3/sheet.component.ts
+++ lesson00/src/app/sheet-v3/sheet.component.ts
@@ -6,10 +6,6 @@ import { sampleLookups } from '../data/sampleLookups';
 import { sampleColumnConfig } from '../data/sampleColumns';
 import { sampleConfig } from '../data/sampleConfig';
 
-
-
-
-
 @Component({
   selector: 'excel-sheet-v3',
   templateUrl: './sheet.component.html',
@@ -18,7 +14,7 @@ import { sampleConfig } from '../data/sampleConfig';
 export class SheetV3Component implements OnInit {
 
   constructor() { }
-
+  /** ngOnInit() - called before render*/
   ngOnInit(): void {
     // init columns
     this.setupColumns()
@@ -26,10 +22,13 @@ export class SheetV3Component implements OnInit {
   }
   // region lesson00
   // input properties
-  
-  @Input() data :any = sampleData;
-  @Input() lookups:any = sampleLookups;
-  @Input() columns: any;
+  // ** data list input **/
+  @Input() data: any = sampleData;
+  // ** pass all lookups as "dict of dict"
+  @Input() lookups: any = sampleLookups;
+  // v3 - passing columns with dropdown 
+  @Input() columns: any = sampleColumnConfig;
+  // extra config
   @Input() config: any;
 
 
@@ -39,14 +38,14 @@ export class SheetV3Component implements OnInit {
   //colNames only in array
 
   colNames: any = []
-
+  /** setup columns if columns paramertes are not set based on data */
   setupColumns() {
     if (!this.data || this.data.length === 0)
       return
     if (this.columns && this.columns.length > 0)
       return
     const item = this.data[0]
-    this.columns=[]
+    this.columns = []
     for (const prop in item) {
       const colDef = { name: prop }
       this.columns.push(colDef)
@@ -55,11 +54,11 @@ export class SheetV3Component implements OnInit {
 
 
   }
-
+  /** return data which can be displayed */
   displayData() {
     return this.data;
   }
-
+  /** */
   displayColumns() {
     return this.columns;
   }
@@ -68,44 +67,70 @@ export class SheetV3Component implements OnInit {
 
   // region lesson02
 
-  currentRow:number= -1
-  currentCol:number= -1
-
+  currentRow: number = -1
+  currentCol: number = -1
+  /**
+   * 
+   * @param i
+   * @param j
+   */
   isCellEdit(i: number, j: number) {
     return i == this.currentRow && j == this.currentCol
   }
-
+  /**
+   * 
+   * @param i
+   * @param j
+   */
   selectSingleCell(i: number, j: number) {
-      this.currentRow=i      
-      this.currentCol =j
+    this.currentRow = i
+    this.currentCol = j
   }
 
-  getDropdown(column:any) {
-    
-    return this.lookups [column.lookup];
+  // region dropdown
+
+  /**
+ * return column lookup 
+ * @param column
+ */
+  getDropdown(column: any) {
+    return this.lookups[column.lookup];
   }
 
-  isDropdown(column:any) {
 
-    return false;
+  /**
+   * check if the column is dropdow
+   * @param column
+   */
+  isDropdown(column: any) {
+    return column.type === 'dropdown' && (!!column.lookup) && this.lookups[column.lookup];
   }
 
-
-  getDisplayValue(column:any, v:any) {
-
+  /**
+   * Display the cell value.
+   * For dropdown get label from dropdown dict
+   * @param column
+   * @param val
+   */
+  getDisplayValue(column: any, val: any) {
     if (this.isDropdown(column)) {
-
-      let dd = this.getDropdown(column);
-      if (dd) {        
-        return dd[v] || v;
+      let dropdown = this.getDropdown(column);
+      if (dropdown) {
+        return dropdown[val] || val;
       }
     }
     else {
-      return v
+      return val
     }
   }
-
-  onKeyEvent($event:KeyboardEvent, src = '',type='') {
+  // endregion dropdown
+  /**
+   * onKeyEvent
+   * @param $event
+   * @param src
+   * @param type
+   */
+  onKeyEvent($event: KeyboardEvent, src = '', type = '') {
 
   }
   //endregion lesson02
